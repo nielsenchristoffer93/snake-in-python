4fd6e1620aa85f02e7bf61319bc7fca3f26cdd7c
Christoffer Nielsen <nielsenchristoffer93@gmail.com>
2021-02-16 13:46:33 +0100

    adding highscore support to project

--- scoreboard.py
+++ scoreboard.py
@@ -13,6 +13,8 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.goto(0, 260)
         self.score = 0
+        with open("data.txt", mode="r") as file:
+            self.high_score = int(file.read())
         self.update_score()
 
     def increase_score(self):
@@ -21,8 +23,16 @@ class Scoreboard(Turtle):
 
     def update_score(self):
         self.clear()
-        self.write(f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score}\tHigh Score: {self.high_score}", move=False, align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", move=False, align=ALIGNMENT, font=FONT)
+    #def game_over(self):
+    #   self.goto(0,0)
+    #   self.write("GAME OVER", move=False, align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(f"{self.high_score}")
+        self.score = 0
+        self.update_score()
